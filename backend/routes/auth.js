09a9a60ddd5c19778b43068ba82eab5f7108750e
shrinivasmandhare7 <shrinivasmandhare7@gmail.com>
2022-12-06 19:38:02 +0530

    bcrypt authentication Added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+var jwt = require('jsonwebtoken');
 
+const JWT_SECRET = 'mostsecuretocken';
 //create a user using :POST "/api/auth/".
 //No login required
 router.post('/createuser', [
@@ -22,12 +25,22 @@ router.post('/createuser', [
             if (user) {
                 return res.status(400).json({ errors: "Sorry user with that email already exists" })
             }
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
+            //create a new user
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password,
+                password: secPass,
             })
-            res.json(user)
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const authTocken = jwt.sign(data, JWT_SECRET);
+            // res.json(user)
+            res.json({ authTocken })
         } catch (error) {
             console.error(error.message);
             res.status(500).send("Some Error Occured")
