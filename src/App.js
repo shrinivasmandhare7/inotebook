b4f4324855a6e0579aa5f00e506f37f3dd1a84b1
shrinivasmandhare7 <shrinivasmandhare7@gmail.com>
2022-12-07 18:53:51 +0530

    1.Created component Home and About 2.Done Setup of React router in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world</h1>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
