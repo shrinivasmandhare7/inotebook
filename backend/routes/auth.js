51c813bd8afcaf1cec5ee46723945250f1114910
shrinivasmandhare7 <shrinivasmandhare7@gmail.com>
2022-12-12 11:30:29 +0530

    Added Signup Component

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -20,7 +20,7 @@ router.post('/createuser', [
         //if there are Errors, return Bad Request and Errors
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
+            return res.status(400).json({ success, errors: errors.array() });
         }
         //check whether user with this email exists already
         try {
