73494d88b9a5f0a5eeb3f4fac4819fce3a44e9ea
shrinivasmandhare7 <shrinivasmandhare7@gmail.com>
2022-12-12 13:38:40 +0530

    Fixed iNoteBook route Bug in the Navbar Component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ const Navbar = () => {
         <>
             <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/Home">iNoteBook</Link>
+                    <Link className="navbar-brand" to="/">iNoteBook</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
